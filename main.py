68a13ce35b17fd96e1bcb36d4b9d27bcdc678d7c
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-06-08 22:14:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', '8BYkEfBA6O6donzWlSihBXox7C0sKR6b')
+app.config['SECRET_KEY'] =  '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('HEROKU_POSTGRESQL_GRAY_URL', "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] =  "sqlite:///blog.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
